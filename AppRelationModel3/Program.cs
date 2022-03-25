5f1b09f4a1e32f4ca257f42698e60bc197ba9fd0
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-03-25 20:57:57 +0300

    Добавьте файлы проекта.

---
+++ AppRelationModel3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppRelationModel3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
